ceb710397d911187b618806f983d9db380dbbb3d
Ben Dean <ben.dean@ontariosystems.com>
2017-01-13 16:28:16 -0500

    BugzID 48639: allow isclib to work with shorter qlist output from older Cache versions
    --HG--
    branch : hg-qlist-fix

--- instance_test.go
+++ instance_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 Ontario Systems
+Copyright 2017 Ontario Systems
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import (
 var _ = Describe("Instance", func() {
 	Context("InstanceFromQList", func() {
 		Context("Invalid qlist", func() {
-			instance, err := isclib.InstanceFromQList("1^2^3^4^5^6^7^8")
+			instance, err := isclib.InstanceFromQList("1^2^3^4^5^6^7")
 			It("Returns an error", func() {
 				Expect(err).To(HaveOccurred())
 			})
@@ -34,6 +34,25 @@ var _ = Describe("Instance", func() {
 			})
 		})
 
+		Context("Valid old qlist", func() {
+			instance, err := isclib.InstanceFromQList("INSTTEST^/ensemble/instances/insttest/^2012.2.3.903.2.12515^down, last used Thu Sep 15 18:58:30 2016^cache.cpf^56772^57772^62972")
+			It("Does not return an error", func() {
+				Expect(err).NotTo(HaveOccurred())
+			})
+			It("Populates the instance properly from a qlist", func() {
+				Expect(instance).ToNot(BeNil())
+				Expect(instance.Name).To(Equal("INSTTEST"), "name")
+				Expect(instance.Directory).To(Equal("/ensemble/instances/insttest/"), "directory")
+				Expect(instance.Version).To(Equal("2012.2.3.903.2.12515"), "version")
+				Expect(instance.Status).To(Equal(isclib.InstanceStatusDown), "status")
+				Expect(instance.Activity).To(Equal("last used Thu Sep 15 18:58:30 2016"), "activity")
+				Expect(instance.CPFFileName).To(Equal("cache.cpf"), "cpf")
+				Expect(instance.SuperServerPort).To(Equal(56772), "ss port")
+				Expect(instance.WebServerPort).To(Equal(57772), "ws port")
+				Expect(instance.JDBCPort).To(Equal(62972), "jdbc port")
+				Expect(instance.State).To(Equal("ok"), "state")
+			})
+		})
 		Context("Valid qlist", func() {
 			instance, err := isclib.InstanceFromQList("INSTTEST^/ensemble/instances/insttest/^2015.2.2.805.0.16216^running, since Fri May 13 22:07:02 2016^cache.cpf^56772^57772^62972^ok^")
 			It("Does not return an error", func() {
